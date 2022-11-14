33fe77e4c76094db6b675f8d4467b1d2bc2d9065
Roberto Albino Junior <junior_wash@hotmail.com>
2022-11-13 22:19:55 -0300

    Ajustando timezone

--- src/main/java/com/roberto/pismo/clients/ClientsApplication.java
+++ src/main/java/com/roberto/pismo/clients/ClientsApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @ComponentScan("com.roberto.pismo.clients")
 @SpringBootApplication
 @Generated
@@ -12,6 +15,11 @@ public class ClientsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientsApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 	}
 
 }
